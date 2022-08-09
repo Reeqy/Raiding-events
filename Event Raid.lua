e52a34dfdff15ac0f843254e106aa718b724af96
Reeqy <109089460+Reeqy@users.noreply.github.com>
2022-08-09 16:49:02 +0100

    Create Event Raid.lua

---
+++ Event Raid.lua
@@ -0,0 +1,50 @@
+--[[
+
+
+
+██████╗░███████╗███████╗░██████╗░██╗░░░██╗██╗░░██╗██╗░░░██╗██████╗░
+██╔══██╗██╔════╝██╔════╝██╔═══██╗╚██╗░██╔╝██║░░██║██║░░░██║██╔══██╗
+██████╔╝█████╗░░█████╗░░██║██╗██║░╚████╔╝░███████║██║░░░██║██████╦╝
+██╔══██╗██╔══╝░░██╔══╝░░╚██████╔╝░░╚██╔╝░░██╔══██║██║░░░██║██╔══██╗
+██║░░██║███████╗███████╗░╚═██╔═╝░░░░██║░░░██║░░██║╚██████╔╝██████╦╝
+╚═╝░░╚═╝╚══════╝╚══════╝░░░╚═╝░░░░░░╚═╝░░░╚═╝░░╚═╝░╚═════╝░╚═════╝░
+
+]]
+
+
+ --(Key is Sophie1)
+
+
+
+
+
+
+ loadstring(game:HttpGet(('https://raw.githubusercontent.com/Reeqy/ReeqyHub.lua/main/Keyproto.lua'),true))()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+ while true do wait(50) 
+  
+ 
+ 
+ 
+     local A_1 = "Want the best script hub? Join .gg/s9C9dWhAqV !!" local A_2 = "All" 
+     local Event = game:GetService("ReplicatedStorage").DefaultChatSystemChatEvents.SayMessageRequest Event:FireServer(A_1, A_2) end
